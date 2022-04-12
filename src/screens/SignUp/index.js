d402f245ece3c31f70db74cd4e96a9be5a65f4b7
soramorikawa <soramorikawa@gmail.com>
2022-04-11 21:05:32 -0500

    firebase authentication and firestore

--- src/screens/SignUp/index.js
+++ src/screens/SignUp/index.js
@@ -1,13 +1,40 @@
 import React, { useState } from "react";
-import { View, Text, FlatList, ImageBackground, Image, Pressable, TextInput, TouchableOpacity } from "react-native";
+import { View, Text, FlatList, ImageBackground, Image, Pressable, TextInput, Alert } from "react-native";
 import styles from "./styles";
 import AntDesign from "react-native-vector-icons/AntDesign";
 import { useNavigation } from '@react-navigation/native';
+import auth, { firebase } from "@react-native-firebase/auth"
+import firestore from "@react-native-firebase/firestore";
 
 
 const SignUpScreen = (props) => {
-  const [inputText, setInputText] = useState('');
   const navigation = useNavigation();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [username, setUsername] = useState('');
+
+  const doCreateUser = async (email, password, firstName, lastName, username) => {
+    try {
+      let response = await auth().createUserWithEmailAndPassword(
+        email,
+        password
+      )
+      firestore().collection('Users').doc(response.user.uid).set({
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        username: username
+      })
+      if (response && response.user) {
+        Alert.alert("Success ✅", "Account created successfully")
+        navigation.navigate("Home")
+      }
+    } catch (e) {
+      console.error(e.message)
+    }
+  }
 
   return (
     <ImageBackground source={require('../../../assets/images/backgroundImage.jpeg')}
@@ -19,6 +46,9 @@ const SignUpScreen = (props) => {
           style={styles.inputText}
           placeholder="First Name:"
           placeholderTextColor="black"
+          onChangeText={text => {
+            setFirstName(text)
+          }}
         />
       </View>
       <View style={styles.inputView} >
@@ -26,6 +56,9 @@ const SignUpScreen = (props) => {
           style={styles.inputText}
           placeholder="Last Name:"
           placeholderTextColor="black"
+          onChangeText={text => {
+            setLastName(text)
+          }}
         />
       </View>
       <View style={styles.inputView} >
@@ -33,6 +66,10 @@ const SignUpScreen = (props) => {
           style={styles.inputText}
           placeholder="Email:"
           placeholderTextColor="black"
+          keyboardType="email-address"
+          onChangeText={text => {
+            setEmail(text)
+          }}
         />
       </View>
       <View style={styles.inputView} >
@@ -40,6 +77,9 @@ const SignUpScreen = (props) => {
           style={styles.inputText}
           placeholder="Username:"
           placeholderTextColor="black"
+          onChangeText={text => {
+            setUsername(text)
+          }}
         />
       </View>
       <View style={styles.inputView} >
@@ -48,9 +88,10 @@ const SignUpScreen = (props) => {
           style={styles.inputText}
           placeholder="Password:"
           placeholderTextColor="black"
+          onChangeText={text => setPassword(text)}
         />
       </View>
-      <Pressable onPress={() => navigation.navigate("Home")}
+      <Pressable onPress={() => doCreateUser(email, password, firstName, lastName, username)}
         style={styles.loginButton1}>
         <Text style={styles.loginText1}>SIGN UP</Text>
       </Pressable>
